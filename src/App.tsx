195751859a1bfce3061f6a40482c68180643de6b
cli3338198 <cli3338198@gmail.com>
2023-10-13 15:03:43 -0700

    fixed infinite loop

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,48 @@
-import { useEffect, useContext, useState, ChangeEvent } from "react";
+import { useEffect, useContext, useState, ChangeEvent, useRef } from "react";
+import { useQuill } from "react-quilljs";
+import "quill/dist/quill.snow.css";
+
 import { socketConnectionContext } from "./context/SocketConnectionManager";
 
 function App() {
   const { socket } = useContext(socketConnectionContext);
   const [textareaValue, setTextareaValue] = useState("");
   const [value, setValue] = useState("");
+
+  // quill stuff
+  const { quill, quillRef } = useQuill();
+
+  const [isReceiving, setIsReceiving] = useState(false);
+  const userChanged = useRef(false);
+
+  // emit
+  useEffect(() => {
+    if (quill && !isReceiving && !userChanged.current) {
+      quill.on("text-change", (delta: any, oldDelta: any, source: any) => {
+        if (source === "user") {
+          userChanged.current = true;
+          // Send the delta/quill contents
+          socket.emit("textChange", JSON.stringify(quill.getContents()));
+        }
+      });
+    }
+  }, [quill, socket, isReceiving]);
+
+  // update quill using socket
+  useEffect(() => {
+    if (quill) {
+      socket.on("textChange", (delta: string) => {
+        setIsReceiving(true);
+        // clear
+        quill.setText("");
+        // replace with new delta
+        quill.updateContents(JSON.parse(delta));
+        setIsReceiving(false);
+        // Reset the userChanged flag after processing the server change
+        userChanged.current = false;
+      });
+    }
+  }, [quill, socket]);
 
   // connect to server
   useEffect(() => {
@@ -39,9 +77,20 @@ function App() {
     <div>
       <textarea
         value={textareaValue}
-        style={{ width: "500px", height: "700px" }}
+        style={{ width: "500px", height: "300px", border: "3px dotted red" }}
       />
       <input value={value} onChange={handleChange} />
+      {/* quill here */}
+
+      <div
+        style={{
+          width: "500px",
+          height: "500px",
+          border: "2px double green",
+        }}
+      >
+        <div ref={quillRef}></div>
+      </div>
     </div>
   );
 }
--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -3,3 +3,8 @@ import { Socket } from "socket.io-client";
 export interface ISocketContext {
   socket: Socket;
 }
+
+export interface IClientToServerEvents {
+  hello: () => void;
+  textChange: (delta: string) => void;
+}
